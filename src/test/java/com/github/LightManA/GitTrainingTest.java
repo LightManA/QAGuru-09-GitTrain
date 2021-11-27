ff779b6157b6233f43ae18c93804cea8ea274cf7
LightManA <AwarenessLightA@gmail.com>
2021-11-28 00:22:01 +0600

    GitTrainingTest class with simple assert created

---
+++ src/test/java/com/github/LightManA/GitTrainingTest.java
@@ -0,0 +1,13 @@
+package com.github.LightManA;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class GitTrainingTest {
+
+    @Test
+    void trainingTest () {
+        Assertions.assertTrue(true);
+    }
+
+}

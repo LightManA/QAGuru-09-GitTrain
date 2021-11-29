b31e282fff78f6c88dadef1babe032b36ca4c8fe
LightManA <AwarenessLightA@gmail.com>
2021-11-30 03:38:54 +0600

    Changed first assert argument to comparison of two numbers 3 > 2

--- src/test/java/com/github/LightManA/GitTrainingTest.java
+++ src/test/java/com/github/LightManA/GitTrainingTest.java
@@ -8,7 +8,7 @@ public class GitTrainingTest {
     @Test
         // Заведомо нерабочий метод
     void secondTest() {
-        Assertions.assertTrue(true);
+        Assertions.assertTrue(3 > 2);
     }
 
     @Test
